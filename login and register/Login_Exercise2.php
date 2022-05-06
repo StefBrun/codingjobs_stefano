ccf5ba401db6efca7c711e85c9865b954406ae25
Brunialti <brunialti.stefano@gmail.com>
2022-05-06 06:59:32 +0200

    add exercice

---
+++ login and register/Login_Exercise2.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+	This exercise is based on the 'login/register' exercise.
+	
+	Now, we have implemented the register/login process into the same project.
+
+	Step 1. Create a 'nav.php' file that contains the two links : register / login
+            Create a link also for the home page (index file)
+            Include this file on all the pages.
+    5Min
+
+    Step 2. Edit 'login.php' page.
+    Once the user is logged-in, create a cookie 'login' to true.
+
+    Step 3.	If the user is loggued-in, hide 'login' & 'register' links.
+
+    Step 4. Create a logout button. When you click on it, it should remove the cookie previously created in the login process.
+
+    Step 5. Hide the logout button when user is not connected.
+    Display the button when user is connected
+
+*/
